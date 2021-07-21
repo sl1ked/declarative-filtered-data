14a0d8f62e699d95c5122514954ea389ee8c3e20
sl1ked <viktor.butenko.00@gmail.com>
2021-07-22 00:06:48 +0300

    test with array input value

--- test/test.js
+++ test/test.js
@@ -39,3 +39,15 @@ test("include values filter with not array's value", () => {
     { id: 4, img: ["1", "2"], src: "" },
   ]);
 });
+test("include values filter with not array's value", () => {
+  const data = [
+    { id: 1 },
+    { id: 2, img: "1" },
+    { id: 3, img: ["1", "3"], src: "" },
+    { id: 4, img: ["1", "2"], src: "" },
+  ];
+  const prop = [{ name: "img", type: "includes", value: ["1", "2"] }];
+  expect(filterData(data, prop)).toEqual([
+    { id: 4, img: ["1", "2"], src: "" },
+  ]);
+});
