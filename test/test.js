6949d8747aee6ad109798a7a763ab568b0009efe
sl1ked <viktor.butenko.00@gmail.com>
2021-07-21 23:42:47 +0300

    include value test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,40 @@
-const filterData=require("../module.js");
+const filterData = require("../module.js");
 
-test("first test:return didn't change data",()=>{
-    const data=[{id:1},{id:2}];
-    expect(filterData(data)).toEqual(data);
+test("first test:return didn't change data", () => {
+  const data = [{ id: 1 }, { id: 2 }];
+  expect(filterData(data)).toEqual(data);
 });
-test("filter is important options",()=>{
-    const data=[{id:1},{id:2},{id:3,img:'1'}];
-    const prop=[{name:"img",type:"have"},]
-    expect(filterData(data,prop)).toEqual([{id:3,img:'1'}]);
+test("filter is important options", () => {
+  const data = [{ id: 1 }, { id: 2 }, { id: 3, img: "1" }];
+  const prop = [{ name: "img", type: "have" }];
+  expect(filterData(data, prop)).toEqual([{ id: 3, img: "1" }]);
 });
-test("double having filter",()=>{
-    const data=[{id:1},{id:2},{id:3,img:'1',src:""},{id:4,img:'1',src:""}];
-    const prop=[{name:"img",type:"have"},{name:"src",type:"have"},]
-    expect(filterData(data,prop)).toEqual([{id:3,img:'1',src:""},{id:4,img:'1',src:""}]);
+test("double having filter", () => {
+  const data = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3, img: "1", src: "" },
+    { id: 4, img: "1", src: "" },
+  ];
+  const prop = [
+    { name: "img", type: "have" },
+    { name: "src", type: "have" },
+  ];
+  expect(filterData(data, prop)).toEqual([
+    { id: 3, img: "1", src: "" },
+    { id: 4, img: "1", src: "" },
+  ]);
+});
+test("include values filter", () => {
+  const data = [
+    { id: 1 },
+    { id: 2, img: ["3"] },
+    { id: 3, img: ["1","3"], src: "" },
+    { id: 4, img: ["1","2"], src: "" },
+  ];
+  const prop = [{ name: "img", type: "include",value:"1" }];
+  expect(filterData(data, prop)).toEqual([
+    { id: 3, img: "1", src: "" },
+    { id: 4, img: "1", src: "" },
+  ]);
 });
