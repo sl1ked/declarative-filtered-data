e6a85b24b17730284cf70d770b1b1b16fb23a4ec
sl1ked <viktor.butenko.00@gmail.com>
2021-07-21 19:07:24 +0300

    change first test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const filterData=require("../module.js");
 
 test("first test:return isn't data",()=>{
-    const data=1;
-    expect(filterData(data).toBe(data));
+    const data=[{id:1},{id:2}];
+    expect(filterData(data)).toEqual(data);
 });
