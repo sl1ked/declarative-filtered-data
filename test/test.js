a5cc8c3e66aac69d608d752c8cee67e13bdba6af
sl1ked <viktor.butenko.00@gmail.com>
2021-07-22 13:45:33 +0300

    test

--- test/test.js
+++ test/test.js
@@ -32,7 +32,9 @@ test("include values filter with not array's value", () => {
     { id: 3, img: ["1", "3"], src: "" },
     { id: 4, img: ["1", "2"], src: "" },
   ];
-  const prop = [{ name: "img", type: "includes", value: "1",strictMode:true }];
+  const prop = [
+    { name: "img", type: "includes", value: "1", strictMode: true },
+  ];
   expect(filterData(data, prop)).toEqual([
     { id: 2, img: "1" },
     { id: 3, img: ["1", "3"], src: "" },
@@ -46,23 +48,36 @@ test("include values filter with array's value strictMode", () => {
     { id: 3, img: ["1", "3"], src: "" },
     { id: 4, img: ["1", "2"], src: "" },
   ];
-  const prop = [{ name: "img", type: "includes", value: ["1", "2"], strictMode:true }];
+  const prop = [
+    { name: "img", type: "includes", value: ["1", "2"], strictMode: true },
+  ];
+  expect(filterData(data, prop)).toEqual([{ id: 4, img: ["1", "2"], src: "" }]);
+});
+test("include values filter with not array's value not  strictMode", () => {
+  const data = [
+    { id: 1 },
+    { id: 2, img: "1" },
+    { id: 3, img: ["1", "3"], src: "" },
+    { id: 4, img: ["1", "2"], src: "" },
+  ];
+  const prop = [
+    { name: "img", type: "includes", value: ["1", "2"], strictMode: false },
+  ];
   expect(filterData(data, prop)).toEqual([
+    { id: 2, img: "1" },
+    { id: 3, img: ["1", "3"], src: "" },
     { id: 4, img: ["1", "2"], src: "" },
   ]);
 });
-test("include values filter with not array's value not  strictMode", () => {
-    const data = [
-      { id: 1 },
-      { id: 2, img: "1" },
-      { id: 3, img: ["1", "3"], src: "" },
-      { id: 4, img: ["1", "2"], src: "" },
-    ];
-    const prop = [{ name: "img", type: "includes", value: ["1", "2"], strictMode:false }];
-    expect(filterData(data, prop)).toEqual([
-        { id: 2, img: "1" },
-        { id: 3, img: ["1", "3"], src: "" },
-        { id: 4, img: ["1", "2"], src: "" },
-    ]);
-  });
-  
+test("check type of checking values", () => {
+  const data = [
+    { id: 1 },
+    { id: 2, img: "1" },
+    { id: 3, img: ["1", "3"], src: "" },
+    { id: 4, img: ["1", "2"], src: "" },
+  ];
+  const prop = [
+    { name: "img", type: "includes", value: ["1", 2], strictMode: true },
+  ];
+  expect(filterData(data, prop)).toEqual([]);
+});
