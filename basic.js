47c3a7f46293056f97232731cb82ee53be3dc1b7
sl1ked <viktor.butenko.00@gmail.com>
2021-07-22 18:16:27 +0300

    add deep object value

--- basic.js
+++ basic.js
@@ -1,4 +1,10 @@
 const isArray = (el) => Array.isArray(el);
 const isObject = (el) => Object.isObject(el);
-
-module.exports={isArray,isObject}
+const deepObjectProperty=(el,way)=>{
+    let result=el;
+    way.split('.').forEach(element => {
+        result=result[element];
+    });
+    return result;
+}
+module.exports={isArray,isObject,deepObjectProperty}
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,6 +1,14 @@
 const filterData = require("../module.js");
+const { isArray, deepObjectProperty } = require("../basic.js");
 
 test("first test:return didn't change data", () => {
   const data = [{ id: 1 }, { id: 2 }];
   expect(filterData(data)).toEqual(data);
+});
+test("check deep property", () => {
+  const obj = { a: { b: { c: 1 } } };
+  const soloObj = { a: 1, c: 2 };
+  expect(deepObjectProperty(obj, "a.b.c")).toEqual(1);
+  expect(deepObjectProperty(soloObj, "a")).toEqual(1);
+  expect(deepObjectProperty(obj, "a.b")).toEqual({ c: 1 });
 });
