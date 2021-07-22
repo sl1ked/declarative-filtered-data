131504e2e7c1bb0660b2fc7bff7b1958179e14f5
sl1ked <viktor.butenko.00@gmail.com>
2021-07-22 14:45:43 +0300

    test custom

---
+++ test/custom.test.js
@@ -0,0 +1,15 @@
+const filterData = require("../module.js");
+
+test("check commad only", () => {
+    const data = [
+      { id: 1 },
+      { id: 2, img: "1" },
+      { id: 3, img: [1, 2], src: "" },
+      { id: 4, img: 1, src: "" },
+    ];
+    const prop = [
+      { name: "img", type: "custom", callback:(el)=>el%2},
+    ];
+    expect(filterData(data, prop)).toEqual([{ id: 4, img: 1, src: "" }]);
+  });
+  
