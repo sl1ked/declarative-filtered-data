0fbda9833102155adda7a766a4d40355b7557151
sl1ked <viktor.butenko.00@gmail.com>
2021-07-22 14:56:31 +0300

    second test who working with context

--- test/custom.test.js
+++ test/custom.test.js
@@ -13,3 +13,16 @@ test("check commad only", () => {
     { id: 4, img: 1, src: "" },
   ]);
 });
+test("check commad only", () => {
+    const data = [
+      { id: 1 },
+      { id: 2, img: ["1"] },
+      { id: 3, img: [1, 2], src: "" },
+      { id: 4, img: [1], src: "" },
+    ];
+    const prop = [{ name: "img", type: "custom", callback: (el) => el.includes(this.value),value:1}];
+    expect(filterData(data, prop)).toEqual([
+      { id: 2, img: "1" },
+      { id: 4, img: 1, src: "" },
+    ]);
+  });
