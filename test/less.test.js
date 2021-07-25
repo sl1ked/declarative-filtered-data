d6553e411f949a66b35d51de11bc46b985ddbc99
sl1ked <viktor.butenko.00@gmail.com>
2021-07-25 20:10:37 +0300

    test for new mode Strict and unStrict mode

--- test/less.test.js
+++ test/less.test.js
@@ -15,3 +15,13 @@ test("less option with string", () => {
     const prop = [{ name: "id", type: "less",value:3 }];
     expect(filterData(data, prop)).toEqual([{ id: 1 }, { id: 2,img:"1" }]);
   });
+  test("less option with different variable", () => {
+    const data = [{ id: 1 }, { id: 2 }, { id: 3}];
+    const prop = [{ name: "id", type: "less",value:2, strict:false}];
+    expect(filterData(data, prop)).toEqual([{ id: 1 }, { id: 2}]);
+  });
+  test("less option with different variable", () => {
+    const data = [{ id: 1 }, { id: 2 }, { id: 3}];
+    const prop = [{ name: "id", type: "less",value:2, strict:true}];
+    expect(filterData(data, prop)).toEqual([{ id: 1 }]);
+  });
