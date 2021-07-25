95b033f1400cfb78e03efcc84dd2f497f67eec18
sl1ked <viktor.butenko.00@gmail.com>
2021-07-25 20:01:08 +0300

    basic test for less option

---
+++ test/less.test.js
@@ -0,0 +1,17 @@
+const filterData = require("../module.js");
+
+test("less option with number", () => {
+  const data = [{ id: 1 }, { id: 2 }, { id: 3, img: "1" }];
+  const prop = [{ name: "id", type: "less",value:2 }];
+  expect(filterData(data, prop)).toEqual([{ id: 1 }]);
+});
+test("less option with string", () => {
+    const data = [{ id: "a" }, { id: "b" }, { id: "d", img: "1" }];
+    const prop = [{ name: "id", type: "less",value:"c" }];
+    expect(filterData(data, prop)).toEqual([{ id: "a" }, { id: "b" }]);
+  });
+  test("less option with different variable", () => {
+    const data = [{ id: "1" }, { id: 1 }, { id: 2, img: "1" }];
+    const prop = [{ name: "id", type: "less",value:3 }];
+    expect(filterData(data, prop)).toEqual([{ id: 1 }, { id: 2,img:"1" }]);
+  });
