edb4eace4b210b0b2478446900cc939eb5aea6e1
sl1ked <viktor.butenko.00@gmail.com>
2021-07-27 00:15:09 +0300

    change  deepObjectProperty to use with array

--- basic.js
+++ basic.js
@@ -2,8 +2,9 @@ const isArray = (el) => Array.isArray(el);
 const isObject = (el) => typeof el==="object"&&typeof el!=="function"&&!isArray(el);
 const deepObjectProperty = (el, way) => {
   let result = el;
+  const copyWay=way.replace(/\[|\]/g,match=>match=="["?".":"");// create object type for array's item: replace [ and ] on space or .
   try {
-    way.split(".").forEach((element) => {
+    copyWay.split(".").forEach((element) => {
       result = result[element];
     });
     return result;
