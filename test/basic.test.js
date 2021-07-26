ccb1f10829a5f25a9b49424238ca20b9ec84a93f
sl1ked <viktor.butenko.00@gmail.com>
2021-07-27 00:23:25 +0300

    test twice arrays option

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,5 +1,5 @@
 const filterData = require("../module.js");
-const { isArray,isObject, deepObjectProperty } = require("../basic.js");
+const { isArray,isObject, getDeepObjectProperty } = require("../basic.js");
 
 test("first test:return didn't change data", () => {
   const data = [{ id: 1 }, { id: 2 }];
@@ -8,11 +8,13 @@ test("first test:return didn't change data", () => {
 test("check deep property", () => {
   const obj = { a: { b: { c: 1 } } };
   const soloObj = { a: 1, c: 2 };
-  expect(deepObjectProperty(obj, "a.b.c")).toEqual(1);
-  expect(deepObjectProperty(soloObj, "a")).toEqual(1);
-  expect(deepObjectProperty(obj, "a.b")).toEqual({ c: 1 });
+  expect(getDeepObjectProperty(obj, "a.b.c")).toEqual(1);
+  expect(getDeepObjectProperty(soloObj, "a")).toEqual(1);
+  expect(getDeepObjectProperty(obj, "a.b")).toEqual({ c: 1 });
 });
 test("check deep property with array's item", () => {
   const obj = { a: [{a:{a:2}}]};
-  expect(deepObjectProperty(obj, "a[0].a")).toEqual({ a: 2 });
+  const obj1 = { a: [{a:[1,2]}]};
+  expect(getDeepObjectProperty(obj, "a[0].a")).toEqual({ a: 2 });
+  expect(getDeepObjectProperty(obj1, "a[0].a[1]")).toEqual(2);
 });
